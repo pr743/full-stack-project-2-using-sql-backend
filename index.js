bca61d52e046b39081c6b0c9245df796ae324412
Prince <princemungra1340@gmail.com>
2026-05-22 17:02:50 +0530

    update page

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ const analyticsRoutes = require("./routes/analyticsRoutes");
 const app = express();
 
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            process.env.FRONTEND_URL
+        ],
+        credentials: true
+    })
+);
 app.use(express.json());
 
 
